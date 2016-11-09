fe766400514045420a00b0d398771dcba4655f47
Skander Jabouzi <skander.jabouzi@geemedia.com>
2016-11-09 17:01:56 -0500

    fix compass rotation

--- compass/ViewController.h
+++ compass/ViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController: UIViewController <CLLocationManagerDelegate> {
     CLLocationManager *_locationManager;
--- compass/ViewController.m
+++ compass/ViewController.m
@@ -10,7 +10,8 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *compassLabel;
-@property (weak, nonatomic) IBOutlet UIImageView *pointerImage;
+
+@property (weak, nonatomic) IBOutlet UIImageView *compassImageView;
 
 @end
 
@@ -31,11 +32,19 @@
 
 - (void) locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading
 {
-    double degrees = newHeading.magneticHeading;
+    /*double degrees = newHeading.magneticHeading;
     double radians = degrees * M_PI / 180;
-    [self pointerImage].transform = CGAffineTransformMakeRotation(radians);
-    [[self compassLabel] setText:[NSString stringWithFormat:@"%d",(int)degrees]];
+    [self compassImageView].transform = CGAffineTransformMakeRotation(radians);
+    [[self compassLabel] setText:[NSString stringWithFormat:@"%d",(int)degrees]];*/
     //NSLog(@"Magnetic Heading: %f", newHeading.magneticHeading);
+    float oldRad =  -manager.heading.trueHeading * M_PI / 180.0f;
+    float newRad =  -newHeading.trueHeading * M_PI / 180.0f;
+    
+    [UIView animateWithDuration:0.2
+                     animations:^{
+                         [self compassImageView].transform = CGAffineTransformMakeRotation(newRad);
+                     }];
+    NSLog(@"%f (%f) => %f (%f)", manager.heading.trueHeading, oldRad, newHeading.trueHeading, newRad);
 }
 
 - (void)didReceiveMemoryWarning {
